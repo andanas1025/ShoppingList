ca84a9acd6adf532fec16f2d89e048954c26d51c
Andres Rubiano Del Chiaro <andres.rubiano@globant.com>
2025-02-11 19:14:20 -0500

    Added lazy column

--- app/src/main/java/com/example/shoppinlist/MainActivity.kt
+++ app/src/main/java/com/example/shoppinlist/MainActivity.kt
@@ -8,13 +8,19 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.shoppinlist.ui.theme.ShoppinListTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,17 +41,32 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(modifier: Modifier = Modifier) {
+    var sItems by remember { mutableStateOf(listOf<ShoppingItem>()) }
     Column(
         modifier = modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
     ) {
         Button(
             onClick = { },
-            modifier = Modifier.align(Alignment.CenterHorizontally)) {
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        ) {
             Text(text = "Add item")
+        }
+        LazyColumn(modifier = Modifier.fillMaxSize().padding(16.dp)) {
+            items(sItems.size) { index ->
+                val item = sItems[index]
+                Text(text = item.name)
+            }
         }
     }
 }
+
+data class ShoppingItem(
+    val id: Int,
+    var name: String,
+    var quantity: Int,
+    var isEditing: Boolean = false
+)
 
 @Preview(showBackground = true)
 @Composable
