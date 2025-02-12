b71e82dba5c767db3d074f7947a541c7409d1032
Andres Rubiano Del Chiaro <andres.rubiano@globant.com>
2025-02-11 19:19:12 -0500

    Moved the code into a different file

--- app/src/main/java/com/example/shoppinlist/MainActivity.kt
+++ app/src/main/java/com/example/shoppinlist/MainActivity.kt
@@ -30,7 +30,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ShoppinListTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
+                    ShoppingListApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -39,39 +39,10 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(modifier: Modifier = Modifier) {
-    var sItems by remember { mutableStateOf(listOf<ShoppingItem>()) }
-    Column(
-        modifier = modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-    ) {
-        Button(
-            onClick = { },
-            modifier = Modifier.align(Alignment.CenterHorizontally)
-        ) {
-            Text(text = "Add item")
-        }
-        LazyColumn(modifier = Modifier.fillMaxSize().padding(16.dp)) {
-            items(sItems.size) { index ->
-                val item = sItems[index]
-                Text(text = item.name)
-            }
-        }
-    }
-}
-
-data class ShoppingItem(
-    val id: Int,
-    var name: String,
-    var quantity: Int,
-    var isEditing: Boolean = false
-)
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ShoppinListTheme {
-        Greeting()
+        ShoppingListApp()
     }
 }
