9ad64a0d358d7d15347b6371c3002be2280edd9d
Andres Rubiano Del Chiaro <andres.rubiano@globant.com>
2025-02-11 19:02:04 -0500

    Added a button in the middle using JC

--- app/src/main/java/com/example/shoppinlist/MainActivity.kt
+++ app/src/main/java/com/example/shoppinlist/MainActivity.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.shoppinlist.ui.theme.ShoppinListTheme
@@ -21,7 +25,6 @@ class MainActivity : ComponentActivity() {
             ShoppinListTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +34,23 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Button(
+            onClick = { },
+            modifier = Modifier.align(Alignment.CenterHorizontally)) {
+            Text(text = "Add item")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ShoppinListTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
