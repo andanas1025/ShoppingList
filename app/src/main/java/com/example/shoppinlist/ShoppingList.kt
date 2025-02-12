5646fe6f54adb70b3c556e9f77517c83af83eef6
Andres Rubiano Del Chiaro <andres.rubiano@globant.com>
2025-02-12 17:27:08 -0500

    Added borderstroke into row

--- app/src/main/java/com/example/shoppinlist/ShoppingList.kt
+++ app/src/main/java/com/example/shoppinlist/ShoppingList.kt
@@ -1,12 +1,17 @@
 package com.example.shoppinlist
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
@@ -18,6 +23,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 data class ShoppingItem(
@@ -48,9 +54,8 @@ fun ShoppingListApp(modifier: Modifier = Modifier) {
                 .fillMaxSize()
                 .padding(16.dp)
         ) {
-            items(sItems.size) { index ->
-                val item = sItems[index]
-                Text(text = item.name)
+            items(sItems) {
+                ShoppingListItem(it, {}, {})
             }
         }
 
@@ -106,5 +111,24 @@ fun ShoppingListApp(modifier: Modifier = Modifier) {
                 }
             )
         }
+    }
+}
+
+@Composable
+fun ShoppingListItem(
+    item: ShoppingItem,
+    onEdit: () -> Unit,
+    onDelete: () -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .border(
+                border = BorderStroke(2.dp, Color(0XFF018786)),
+                shape = RoundedCornerShape(20)
+            )
+    ) {
+        Text(text = item.name, modifier = Modifier.padding(8.dp))
     }
 }
